441c9e015b7160ab84db6c982d1f302d9b5beab3
mingsang <minsang0850@naver.com>
2023-12-11 15:43:54 +0900

    git init

---
+++ src/main/java/minsang/searchblog/SearchBlogApplication.java
@@ -0,0 +1,13 @@
+package minsang.searchblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/minsang/searchblog/SearchBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package minsang.searchblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
